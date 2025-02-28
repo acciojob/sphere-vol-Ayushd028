ed5ee134688b406ffcadca023859634de8bc10bc
Ayush Dwivedi <dwivediayush028@gmail.com>
2025-02-28 17:55:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
 function volume_sphere() {
-    //Write your code here
-  
+	let radiusInput = document.querySelector("#radius").value;
+	let radius = parseFloat(radiusInput);
+
+	if(isNaN(radius) || radius<0){
+		document.querySelector("#volume").value = "Nan";
+		return false;
+	}
+	
+    let volume = (4 / 3) * Math.PI * Math.pow(radius, 3);
+	document.querySelector("#volume").value = volume.toFixed(4);
+	return false;
 } 
 
 window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
